2e9aea016606148ac4d1e39865709a6c6d14e4bf
Abhishek Katiyar <abhishekkatiyar049@gmail.com>
2019-01-13 19:35:43 +0530

    add test case for checking the extraction of second row

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -2,11 +2,15 @@ require 'matrix.rb'
 describe Matrix do
 
   context "when extracting a row of a matrix" do
-    it "returns [1, 2] for the matrix \n1 2\n10 20" do
+    it "returns [1, 2] as the first row of the matrix \n1 2\n10 20" do
       matrix = Matrix.new("1 2\n10 20")
-      expect(matrix.extract_row(0)).to eq([1, 2])
+      expect(matrix.extract_row(0)).to eq([1 , 2])
     end
 
+    it "returns [8, 6] as the second row of the matrix \n9 7\n8 6" do
+      matrix = Matrix.new("9 7\n8 6")
+      expect(matrix.extract_row(1)).to eq([8 , 6])
+    end
   end
 end
 
