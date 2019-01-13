4d58f9dd358433b00562edf6b7bd3e4517034557
Abhishek Katiyar <abhishekkatiyar049@gmail.com>
2019-01-13 19:48:13 +0530

    add test for extraction of row from a non-regular matrix

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -11,6 +11,12 @@ describe Matrix do
       matrix = Matrix.new("9 7\n8 6")
       expect(matrix.extract_row(1)).to eq([8 , 6])
     end
+
+    it "returns [9, 8, 7] as the first row of the matrix\n9 8 7\n19 18 17" do
+      matrix = Matrix.new("9 8 7\n19 18 17")
+      expect(matrix.extract_row(0)).to eq([9, 8, 7])
+    end
   end
+
 end
 
