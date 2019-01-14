6f23c1e3e80f1555d7b80b8ac71e01ef602f7e5c
Abhishek Katiyar <abhishekkatiyar049@gmail.com>
2019-01-14 11:02:00 +0530

    correct format for matrix_spec.rb

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -4,12 +4,12 @@ describe Matrix do
   context "when extracting a row of a matrix" do
     it "returns [1, 2] as the first row of the matrix \n1 2\n10 20" do
       matrix = Matrix.new("1 2\n10 20")
-      expect(matrix.extract_row(0)).to eq([1 , 2])
+      expect(matrix.extract_row(0)).to eq([1, 2])
     end
 
     it "returns [8, 6] as the second row of the matrix \n9 7\n8 6" do
       matrix = Matrix.new("9 7\n8 6")
-      expect(matrix.extract_row(1)).to eq([8 , 6])
+      expect(matrix.extract_row(1)).to eq([8, 6])
     end
 
     it "returns [9, 8, 7] as the first row of the matrix\n9 8 7\n19 18 17" do
@@ -21,12 +21,12 @@ describe Matrix do
   context "when extracting a column of a matrix" do
     it "returns [1 , 10] as the first column of the matrix \n1 2\n10 20" do
       matrix = Matrix.new("1 2\n10 20")
-      expect(matrix.extract_column(0)).to eq([1 , 10])
+      expect(matrix.extract_column(0)).to eq([1, 10])
     end
 
     it "returns [5 , 20] as the second column of the matrix \n4 5\n19 20" do
       matrix = Matrix.new("4 5\n19 20")
-      expect(matrix.extract_column(1)).to eq([5 , 20])
+      expect(matrix.extract_column(1)).to eq([5, 20])
     end
 
     it "returns [19, 13] as the second column of the non-regular matrix\n10 19 15\n11 13 16" do
